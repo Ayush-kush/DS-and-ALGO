9f1d73b499579ac2a7e7b9cb80da6de5f79a81f5
Ayush-kush <ayushkushwah678@gmail.com>
2026-07-30 14:09:55 +0530

    Day 1 Problems done

--- Arrays/array1.cpp
+++ Arrays/array1.cpp
@@ -86,7 +86,7 @@ using namespace std;
 
 // Best O(N)
 
-int main(int argc, char const *argv[])
+// int main(int argc, char const *argv[])
 {
     int t;
     cin>>t;
@@ -121,4 +121,114 @@ int main(int argc, char const *argv[])
     cout<<secondlargest;
     }
     return 0;
+}
+
+// Another Solution found on my own
+// int main(){
+//     int t;
+//     cin>>t;
+    
+//     while (t--)
+//     {
+//         int n;
+//         cin>>n;
+//         int arr[n];
+        
+//          for (int i = 0; i < n; i++)
+//          {
+//              cin>>arr[i];
+//          }
+
+//         int large = arr[n-1];
+//         int secondLarge = arr[0];
+
+//         int i = 1 ;
+//         int j = n-1 ;
+
+//         while(i<j){
+
+
+//         	if (large<arr[j])
+//         	{
+//         		large=arr[j];
+//         	}
+//         	if(secondLarge < arr[i]){
+//         		secondLarge=arr[i];
+//         	}
+//         	if (large<secondLarge)
+//         	{
+//         		swap(large,secondLarge);
+//         	}
+//         	i++;
+//         	j--;
+//         }
+//         cout<<secondLarge;
+//     }
+//     return 0;
+// }  
+
+// Q2. Check if array is sorted
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+    int t;
+    cin>>t;
+    
+    // while (t--)
+    // {
+    //     int n;
+    //     cin>>n;
+    //     int arr[n];
+        
+    //     // 1. Brute Force
+    //      for (int i = 0; i < n; i++)
+    //      {
+    //          cin>>arr[i];
+    //      }
+
+    //      set<int> st;
+    //      for (int i = 0; i < n; ++i)
+    //      {
+    //          st.insert(arr[i]);
+    //      }
+
+    //      cout<<st.size();
+    // }
+    
+    //2. Better
+
+    while (t--)
+    {
+        int n;
+        cin>>n;
+        int arr[n];
+        
+        
+         for (int i = 0; i < n; i++)
+         {
+             cin>>arr[i];
+         }
+
+         int index = 0;
+         
+         for (int i = 1; i < n; i++)
+         {
+                if(arr[i]>arr[index]){
+                    index+=1;
+                    arr[index]=arr[i];
+                }
+         }
+
+        for (int i = 0; i <= index; i++)
+         {
+             cout<<arr[i];
+         }
+         
+    }
+
+    return 0;
+
 }
